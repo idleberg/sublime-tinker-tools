2b53633e2e2caa35d6f4e08369f3d51b3e5b3215
idleberg <git@idleberg.com>
2019-04-06 01:06:53 +0200

    refactor

--- bin/glue.js
+++ bin/glue.js
@@ -3,94 +3,20 @@
 const meta = require('../package.json');
 
 const program = require('commander');
-const fs = require('fs');
-const glob = require('glob');
-const xmlJs = require('xml-js');
+const { glue } = require('../lib');
 
 program
-    .version(meta.version)
-    .description('Joins Sublime Text snippets into completions')
-    .arguments('<files>')
-    .usage('<files> [options]')
-    .option('-i, --indent [level]', 'specify indentation level')
-    .option('-s, --scope [scope]', 'override default syntax scope')
-    .action( pattern => {
+  .version(meta.version)
+  .description('Joins Sublime Text snippets into completions')
+  .arguments('<files>')
+  .usage('<files> [options]')
+  .option('-i, --indent [level]', 'specify indentation level')
+  .option('-s, --scope [scope]', 'override default syntax scope')
+  .parse(process.argv);
 
-        let i = 0;
-        let scopes = [];
-        let output = {
-            'generator': `${meta.name} - ${meta.homepage}`,
-            'scope': null,
-            'completions': []
-        };
+const options = {
+  indent: parseInt(program.indent) || 4,
+  scope: program.scope ? program.scope : null
+};
 
-        glob(pattern, (error, files) => {
-
-            files.forEach( filePath => {
-                let data;
-                let obj;
-
-
-                // Read file
-                try {
-                    data = fs.readFileSync(filePath);
-                } catch (error) {
-                    return console.error(`Error: Can't read file "${filePath}"`);
-                }
-
-
-                // Validate XML
-                try {
-                    obj = xmlJs.xml2js(data, {
-                        spaces: 4,
-                        compact: true
-                    });
-                } catch (error) {
-                    throw error;
-                }
-
-                scopes.push(obj.snippet.scope._text);
-
-                let trigger = obj.snippet.tabTrigger._text;
-                let contents = obj.snippet.content._cdata;
-                let description = null;
-
-                if (typeof obj.snippet.description !== 'undefined') {
-                    description = obj.snippet.description._text || null;
-                    if (description !== null) {
-                        trigger = `${trigger}\t${description}`;
-                    }
-                }
-
-                output.completions[i] = {
-                    trigger: trigger,
-                    contents: contents
-                };
-
-                i++;
-            });
-
-            if (scopes.unique.length > 1 && typeof program.scope === 'undefined') {
-                return console.error('Error: Snippets containing multiple scopes can\'t be joined ' + files);
-            }
-
-            if (typeof program.scope !== 'undefined' && program.scope.length > 0) {
-                output.scope = program.scope;
-            } else {
-                output.scope = scopes[0];
-            }
-
-            let indent = parseInt(program.indent) || 4;
-
-            console.log(JSON.stringify(output, null, indent));
-        });
-    })
-.parse(process.argv);
-
-if (program.args.length === 0) program.help();
-
-Array.prototype.unique = () => {
-  return this.filter( (value, index, self) => {
-    return self.indexOf(value) === index;
-  });
-}
+glue(program.args, options);
--- bin/scissors.js
+++ bin/scissors.js
@@ -2,12 +2,8 @@
 
 const meta = require('../package.json');
 
-const fs = require('fs');
-const path = require('path');
 const program = require('commander');
-const process = require('process');
-const sanitize = require('sanitize-filename');
-const xmlJs = require('xml-js');
+const { scissors } = require('../lib');
 
 program
     .version(meta.version)
@@ -17,99 +13,12 @@ program
     .option('-s, --scope [scope]', 'override default syntax scope')
     .option('-o, --output [directory]', 'specify default output directory')
     .option('-i, --indent [level]', 'specify indentation level')
-    .action( file => {
-
-        fs.readFile(file, async (err, data) => {
-            if (err) throw err;
-
-            let input;
-            let outputDir = await makeOutDir(file);
-
-            try {
-                input = JSON.parse(data.toString());
-            } catch (err) {
-                return console.error(`Could not read ${file}, invalid JSON?`);
-            }
-
-            if (typeof program.scope !== 'undefined' && program.scope !== null) {
-                console.log(`Overriding syntax scope to "${program.scope}"`);
-            }
-
-            Object.keys(input.completions).forEach( key => {
-                let indent = parseInt(program.indent) || 4;
-                let scope = program.scope || input.scope;
-                let contents = input.completions[key].contents;
-                let trigger = input.completions[key].trigger;
-                let description = '';
-
-                if (typeof contents === 'undefined' || contents === null || typeof trigger === 'undefined' || trigger === null) {
-                    return;
-                }
-
-                // Split tab-separated description
-                if (trigger.indexOf('\t') !== -1 ) {
-                    let tabs = trigger.split('\t');
-                    if (tabs.length > 2) {
-                        console.warn('Conversion aborted, trigger "' + trigger + '" contains multiple tabs');
-                    }
-                    trigger = tabs[0];
-                    description = tabs.slice(-1).pop();
-                }
-
-                let obj = {
-                    _comment: ` ${meta.name} - ${meta.homepage} `,
-                    snippet: {
-                        content: {
-                            _cdata: contents
-                        },
-                        tabTrigger: {
-                            _text: trigger
-                        },
-                        description: {
-                            _text: description
-                        },
-                        scope: {
-                            _text: scope
-                        }
-                    }
-                };
-
-                let output = xmlJs.js2xml(obj, {
-                    compact: true,
-                    spaces: indent
-                });
-
-                let outputFile = sanitize(trigger) + '.sublime-snippet';
-                let outputFullPath = path.join(outputDir, outputFile);
-
-                fs.writeFile(outputFullPath, output, err => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log(`Writing "${outputFile}"`);
-                });
-            });
-        });
-    })
-
     .parse(process.argv);
 
-if (program.args.length === 0) program.help();
+const options = {
+  indent: parseInt(program.indent) || 4,
+  output: program.output ? program.output : null,
+  scope: program.scope ? program.scope : null
+};
 
-// Functions
-const makeOutDir = async (file) => {
-    let outputDir;
-    let basename = path.basename(file, path.extname(file));
-
-    if (typeof program.output === 'undefined' || program.output === null) {
-        outputDir = path.join(process.cwd(), basename);
-    } else {
-        outputDir = program.output;
-    }
-
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir);
-    }
-
-    return outputDir;
-}
+scissors(program.args[0], options);
